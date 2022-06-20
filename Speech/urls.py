8a28db0e07fad597c5d497e8558e7846addf6ef8
chnk58hoang <19021285@vnu.edu.vn>
2022-06-20 20:43:53 +0700

    fix_predict_error

--- Speech/urls.py
+++ Speech/urls.py
@@ -2,5 +2,5 @@ from django.urls import path,include
 from .views import *
 
 urlpatterns = [
-    path('predict/',Predict.as_view(),name='predict')
+    path('predict/',Predict.as_view(),name='predict') #url http://127.0.0.1:8000/speech/predict/
 ]
--- Speech/views.py
+++ Speech/views.py
@@ -5,13 +5,13 @@ from .apps import SpeechConfig
 from .serializers import *
 import tensorflow as tf
 import numpy as np
+import librosa
 import keras
 
 
 # Create your views here.
 
 class Predict(APIView):
-
     @staticmethod
     def extract_log_melspectrogram(waveform, sr=16000):
         stfts = tf.signal.stft(waveform, frame_length=1024, frame_step=256, fft_length=1024)
@@ -48,14 +48,20 @@ class Predict(APIView):
         return output_text
 
     @staticmethod
-    def predict(log_mel_spectrogram):
+    def predict_label(log_mel_spectrogram):
         pred = SpeechConfig.model.predict(log_mel_spectrogram)
         return pred
 
     def post(self, request):
         audio = request.data['audio']
-        log_mel_spectrogram = self.extract_log_melspectrogram(waveform=audio)
-        pred = self.predict(log_mel_spectrogram)
+        audio = tf.cast(audio,dtype=tf.float32)
+        #audio = tf.expand_dims(audio,axis=0)
+
+
+        log_mel_spectrogram = self.extract_log_melspectrogram(waveform=audio,sr=16000)
+        pred = self.predict_label(log_mel_spectrogram[tf.newaxis])
         decoded_pred = self.decode_batch_predictions(pred)
 
-        return Response(decoded_pred)
+        prediction = " ".join(decoded_pred)
+
+        return Response(prediction)
